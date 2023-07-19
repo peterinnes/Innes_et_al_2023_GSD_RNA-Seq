c44d7ad9cc66b90396767cc805e0c44f93fce637
Peter Innes <ptrinnes@gmail.com>
2023-07-19 09:42:35 -0600

    remove scripts no longer in use

--- scripts/not_in_use/STAR_genomeGenerate_stringtie.sh
+++
@@ -1,9 +0,0 @@
-# Generate genome index file for STAR alignments, this time using the annotations produced by stringtie
-
-STAR \
-	--runThreadN 16 \
-	--runMode genomeGenerate \
-	--genomeDir /data3/peter/sunflower/stringtie/STAR_genome_indices \
-	--genomeFastaFiles /data3/peter/sunflower/ref_genome_Ha412HO/Ha412HOv2.0-20181130.fasta \
-	--sjdbGTFfile /data3/peter/sunflower/stringtie/merged_transcripts.stringtie.gtf \
-	--sjdbOverhang 74 
